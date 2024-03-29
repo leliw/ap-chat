0eeaff038f990dbcb31874cd8175288113ac69f5
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-03-29 13:05:02 +0100

    Update chat component with connection status and loading spinner

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,34 +1,54 @@
-import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ChatService, Message } from './chat.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @Component({
     selector: 'app-chat',
     standalone: true,
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, MatIconModule, MatTooltipModule],
     templateUrl: './chat.component.html',
     styleUrl: './chat.component.css'
 })
-export class ChatComponent implements OnInit, AfterViewChecked {
+export class ChatComponent implements OnInit, OnDestroy, AfterViewChecked {
 
     @ViewChild('container') container!: ElementRef;
 
     messages: Message[] = []
     newMessage = '';
+    waitingForResponse = false;
+    connected = false;
 
     constructor(private wsService: ChatService) { }
 
     ngOnInit(): void {
         this.wsService.connect()
-            .subscribe(msg => this.messages.push(msg));
+            .subscribe({
+                next: (msg: Message) => {
+                    this.waitingForResponse = false;
+                    this.messages.push(msg);
+                },
+                error: (err: any) => {
+                    this.messages.push({ channel: "error", text: "Błąd połączenia z serwerem.\nSpróbuj ponownie później." });
+                    console.error(err);
+                },
+                complete: () => console.warn('Completed!')
+            });
+        this.wsService.connected().subscribe(connected => this.connected = connected);
+    }
+
+    ngOnDestroy(): void {
+        this.wsService.disconnect();
     }
 
     sendMessage(): void {
         this.messages.push({ channel: "user", "text": this.newMessage });
         if (this.newMessage.trim().length > 0) {
             this.wsService.send(this.newMessage);
+            this.waitingForResponse = true;
             this.newMessage = '';
         }
     }
--- frontend/src/app/chat/chat.service.ts
+++ frontend/src/app/chat/chat.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject, Subscription, interval } from 'rxjs';
 import { WebSocketSubject } from 'rxjs/webSocket';
 
 export interface Message {
@@ -9,9 +10,17 @@ export interface Message {
 @Injectable({
     providedIn: 'root'
 })
+/**
+ * Represents a service for handling chat functionality.
+ */
 export class ChatService {
 
-    private socket$: WebSocketSubject<Message>;
+    private socket$!: WebSocketSubject<Message>;
+    private proxy$ = new Subject<Message>();
+    private connected$ = new BehaviorSubject<boolean>(false);
+    private socketUrl: string;
+    private subInterval: Subscription
+
     private endpoint = '/api/ws';
 
     constructor() {
@@ -20,20 +29,74 @@ export class ChatService {
         let protocol = url.protocol === 'https:' ? 'wss' : 'ws';
         // Replace the port of the host if the app is running in development mode
         host = host.replace('localhost:4200', 'localhost:8000')
-        const socketUrl = `${protocol}://${host}${this.endpoint}`;
-        this.socket$ = new WebSocketSubject(socketUrl);
+        this.socketUrl = `${protocol}://${host}${this.endpoint}`;
+        this.socket$ = this.createSocket();
+        this.subInterval = interval(5000).subscribe(val => this.reconnectSocket());
     }
 
-    public connect(): WebSocketSubject<Message> {
-        return this.socket$;
+    /**
+     * Creates a WebSocketSubject for sending and receiving messages.
+     * @returns The WebSocketSubject instance.
+     */
+    private createSocket(): WebSocketSubject<Message> {
+        const socket$ = new WebSocketSubject<Message>({
+            url: this.socketUrl,
+            openObserver: { next: () => this.connected$.next(true) },
+            closeObserver: { next: () => this.connected$.next(false) }
+        });
+        socket$.subscribe({
+            next: (message) => this.proxy$.next(message),
+            error: (error) => console.error(error)
+        });
+        return socket$;
     }
 
+    /**
+     * Reconnects the socket if it is not already connected.
+     */
+    private reconnectSocket() {
+        if (!this.connected$.value && this.socket$)
+            this.socket$ = this.createSocket();
+    }
+
+    /**
+     * Connects to the chat service and returns a subject that emits messages.
+     * @returns A subject that emits messages.
+     */
+    public connect(): Subject<Message> {
+        return this.proxy$;
+    }
+
+    /**
+     * Disconnects the chat service.
+     */
+    public disconnect(): void {
+        this.close();
+    }
+
+    /**
+     * Sends a message through the socket.
+     * @param message - The message to be sent.
+     */
     public send(message: any): void {
         this.socket$.next(message);
     }
 
+    /**
+     * Closes the chat service.
+     * Unsubscribes from the subInterval and completes the socket$.
+     */
     public close(): void {
+        this.subInterval.unsubscribe();
         this.socket$.complete();
     }
-    
+
+    /**
+     * Returns an Observable that emits a boolean value indicating whether the chat service is connected.
+     * @returns An Observable that emits a boolean value indicating the connection status.
+     */
+    public connected(): Observable<boolean> {
+        return this.connected$;
+    }
+
 }
